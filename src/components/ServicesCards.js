596323721a55cfcca6cf2b7ffc8e0a01ae5a55dc
Lev_Gordon <lev.gordonsa@gmail.com>
2022-09-04 20:09:20 -0700

    Service page design

---
+++ src/components/ServicesCards.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import CardItem from './CardItem'
+
+import "./ServicesCards.css"
+
+function ServicesCards() {
+
+ 
+  return (
+    <div className='cards'>
+        <h4> TRVL offers many different services to attend all of your needs! </h4>
+        <div className='cards__container'>
+            <div className='cards__wrapper'>
+                <ul className='cards__items'>
+                    <CardItem 
+                        src='images/airplane.jpeg'
+                        text="We book your airplane tickets at the best rates."
+                        label="Savings"
+                        path='/sign-up'
+                    />
+                    <CardItem 
+                        src='images/Kilimanjaro.webp'
+                        text="We offer trips to the most unique locations in the world"
+                        label="Adventure"
+                        path='/sign-up'
+                    />
+                     <CardItem 
+                        src='images/mountain-biker.jpg'
+                        text="We offer adventure plans for each location, guaranteeing a thrilling experience."
+                        label="Memories for life"
+                        path='/sign-up'
+                    />
+                </ul>
+                <ul className='cards__items'>
+                    <CardItem 
+                        src='images/hotel.jpg'
+                        text="We will book a hotel to suit your adventure and needs."
+                        label="Perfect stay"
+                        path='/sign-up'
+                    />
+                     <CardItem 
+                        src='images/traveltourism-1.jpg'
+                        text="We take on all of the paperwork - we just need your signature."
+                        label="No hassle"
+                        path='/sign-up'
+                    />
+                    <CardItem 
+                        src='images/tour-guide.jpg'
+                        text="TRVL has connections with many tour-agencies around the world."
+                        label="Cultural experience"
+                        path='/sign-up'
+                    />
+                </ul>        
+            </div> 
+
+        </div>
+    </div>
+  )
+}
+
+export default ServicesCards
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,9 +1,24 @@
-import React from 'react'
-import '../../App.css'
- 
+import React from "react";
+import "../../App.css";
+import Footer from "../Footer";
+import ServicesCards from "../ServicesCards";
+
+
+
+
 
 function Services() {
-  return <h1 className='services'>SERVICES</h1>
+
+
+
+  return (
+    <div>
+      <h1 className="services">SERVICES</h1>
+      <ServicesCards />
+      {/* <h1> Sign up with TRVL and start planning your next adventure! </h1> */}
+      <Footer />
+    </div>
+  );
 }
 
-export default Services
+export default Services;
