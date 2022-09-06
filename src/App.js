8c6b825f36476354043ebe8caef7cf6c22d7c0bf
Lev_Gordon <lev.gordonsa@gmail.com>
2022-09-05 22:32:15 -0700

    changed products to destinations

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from './components/pages/Home.js'
 import Services from './components/pages/Services.js'
-import Products from './components/pages/Products.js'
 import SignUp from './components/pages/SignUp.js'
+import Destinations from './components/pages/Destinations';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home/>}/>
           <Route path="/services" element={<Services/>}/>
-          <Route path="/products" element={<Products/>}/>
+          <Route path="/destinations" element={<Destinations/>}/>
           <Route path="/sign-up" element={<SignUp/>}/>
 
         </Routes>
---
+++ src/components/pages/Destinations.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../../App.css'
+ 
+
+function Destinations() {
+  return <h1 className='destinations'>DESTINATIONS</h1>
+}
+
+export default Destinations
