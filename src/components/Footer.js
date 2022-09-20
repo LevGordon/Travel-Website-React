a5286d2a3eef976c8aa8da6eb4847ce0f4d02239
Lev_Gordon <lev.gordonsa@gmail.com>
2022-09-19 18:20:26 -0700

    re-did the footer design and code structure

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,27 +7,28 @@ function Footer() {
   return (
     <div className="footer-container">
       <section className="footer-subcription">
-        <p className="footer-subscription-heading">
-          Join the Adventure newsletter to receive our best vacation deals
-        </p>
-        <p className="footer-subscription-text">
-          You can unsubscribe at any time.
-        </p>
-        <div className="input-areas">
-          <form>
-            <input
-              className="footer-input"
-              type="email"
-              name="email"
-              placeholder="Your Email Here"
-            />
-            <Button buttonStyle="btn--outline">Subscribe!</Button>
-          </form>
+        <div className="subscribe-wrap">
+          <p className="footer-subscription-heading">
+            Join the Adventure newsletter to receive our best vacation deals
+          </p>
+          <p className="footer-subscription-text">
+            You can unsubscribe at any time.
+          </p>
+          <div className="input-areas">
+            <form>
+              <input
+                className="footer-input"
+                type="email"
+                name="email"
+                placeholder="Your Email Here"
+              />
+              <Button buttonStyle="btn--outline">Subscribe!</Button>
+            </form>
+          </div>
         </div>
       </section>
       <div className="footer-links">
-        <div className="footer-links-wrapper">
-          <div class="footer-link-items">
+          <div className="footer-link-items">
             <h2>About Us</h2>
             <Link to="/sign-up">How it works</Link>
             <Link to="/">Testimonials</Link>
@@ -35,30 +36,27 @@ function Footer() {
             <Link to="/">Investors</Link>
             <Link to="/">Terms of Service</Link>
           </div>
-          <div class="footer-link-items">
+          <div className="footer-link-items">
             <h2>Contact Us</h2>
             <Link to="/">Contact</Link>
             <Link to="/">Support</Link>
             <Link to="/">Destinations</Link>
             <Link to="/">Sponsorships</Link>
           </div>
-        </div>
-        <div className="footer-link-wrapper">
-          <div class="footer-link-items">
+          <div className="footer-link-items">
             <h2>Videos</h2>
             <Link to="/">Submit Video</Link>
             <Link to="/">Ambassadors</Link>
             <Link to="/">Agency</Link>
             <Link to="/">Influencer</Link>
           </div>
-          <div class="footer-link-items">
+          <div className="footer-link-items">
             <h2>Socials</h2>
             <Link to="/">Instagram</Link>
             <Link to="/">Facebook</Link>
             <Link to="/">Youtube</Link>
             <Link to="/">Twitter</Link>
           </div>
-        </div>
       </div>
       <section className="social-media">
         <div className="social-media-wrap">
@@ -73,36 +71,36 @@ function Footer() {
                     <i className="fab fa-facebook-f" />
                 </Link>
                 <Link
-              class='social-icon-link instagram'
+              className='social-icon-link instagram'
               to='/'
               target='_blank'
               aria-label='Instagram'
             >
-              <i class='fab fa-instagram' />
+              <i className='fab fa-instagram' />
             </Link>
             <Link
-              class='social-icon-link youtube'
+              className='social-icon-link youtube'
               to='/'
               target='_blank'
               aria-label='Youtube'
             >
-              <i class='fab fa-youtube' />
+              <i className='fab fa-youtube' />
             </Link>
             <Link
-              class='social-icon-link twitter'
+              className='social-icon-link twitter'
               to='/'
               target='_blank'
               aria-label='Twitter'
             >
-              <i class='fab fa-twitter' />
+              <i className='fab fa-twitter' />
             </Link>
             <Link
-              class='social-icon-link twitter'
+              className='social-icon-link twitter'
               to='/'
               target='_blank'
               aria-label='LinkedIn'
             >
-              <i class='fab fa-linkedin' />
+              <i className='fab fa-linkedin' />
             </Link>
             </div>
         </div>
